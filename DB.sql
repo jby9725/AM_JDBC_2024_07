14c8ed12eeaffed33918c990db99419e930c65a9
jby9725 <jby9725@gmail.com>
2024-07-16 11:23:04 +0900

    DB.sql 갱신

--- DB.sql
+++ DB.sql
@@ -1,28 +1,3 @@
--- ArticleManager Data
-CREATE DATABASE ArticleManager;
-USE ArticleManager;
-
-DROP TABLE article;
-
-CREATE TABLE article (
-    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `regDate` DATETIME NOT NULL,
-    `updateDate` DATETIME NOT NULL,
-    `title` VARCHAR(50) NOT NULL,
-    `body` TEXT
-);
-
-INSERT INTO article
-SET title = '제목1', `body` = '내용1';
-
-SELECT * FROM article;
-
-INSERT INTO article
-SET regDate = NOW(),
-updateDate = NOW(),
-title = '제목1',
-`body` = '내용1';
-
 -- 문자열 붙이기
 SELECT CONCAT('제목' , ' 1');
 -- 랜덤 수 출력 (범위 : 0~1)
@@ -30,15 +5,17 @@ SELECT RAND();
 
 SELECT SUBSTRING(RAND() * 1000 FROM 1 FOR 2);
 
--- 문자열 붙이기 + 랜덤 수 출력
-## author 붙이기 전
-INSERT INTO article
-SET regDate = NOW(),
-    updateDate = NOW(),
-    title = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),
-    `body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));
+#####################################################################
+## 구조 변경 (author 추가)
+CREATE TABLE article (
+    `id` INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `regDate` DATETIME NOT NULL,
+    `updateDate` DATETIME NOT NULL,
+    `author` INT UNSIGNED NOT NULL,
+    `title` VARCHAR(50) NOT NULL,
+    `body` TEXT NOT NULL
+);
 
-#########################################################################
 -- `member` table 생성
 CREATE TABLE `member` (
     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -49,32 +26,34 @@ CREATE TABLE `member` (
     `nickname` VARCHAR(100) NOT NULL
 );
 
-DESC `article`;
-DESC `member`;
-
 INSERT INTO `member` (`regDate`, `userId`, `password`, `nickname`)
 VALUES
     (NOW(), 'test01', 'test01', '홍길동'),
     (NOW(), 'test02', 'test02', '홍길순'),
     (NOW(), 'test03', 'test03', '임꺽정');
 
-SELECT * FROM article;
-SELECT * FROM `member`;
-
-# TODO
-# 작성자(article list, article detail ## 수정, 삭제 권한 체크
-ALTER TABLE `article` ADD `author` int NOT NULL;
-
-ALTER TABLE `article` ADD CONSTRAINT FK_autor FOREIGN KEY(author)
-    REFERENCES `member`(id) ON DELETE set null; ON UPDATE set null;
-
 -- 문자열 붙이기 + 랜덤 수 출력
-## author 붙이기 전
-
-
 INSERT INTO article
 SET regDate = NOW(),
     updateDate = NOW(),
+    author = 3,
     title = CONCAT('제목', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),
-    `body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2)),
-    `author` = 1;
+    `body` = CONCAT('내용', SUBSTRING(RAND() * 1000 FROM 1 FOR 2));
+
+
+SELECT * FROM article;
+SELECT * FROM `member`;
+
+## select ALL
+SELECT A.*, M.nickname
+FROM article A
+INNER JOIN `member` M
+ON A.author = M.id
+ORDER BY A.id DESC;
+
+## id 옵션 추가해서 select
+SELECT A.*, M.nickname
+FROM article A
+INNER JOIN `member` M
+ON A.author = M.id
+WHERE A.id = 1;
